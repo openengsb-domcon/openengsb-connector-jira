38f820ae053d1e482ab6b382ce2b75ab19109687
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2011-07-25 16:10:57 +0200

    [OPENENGSB-1838] now the sendEvent doesn't need the role field for the events any more

--- src/main/java/org/openengsb/connector/jira/internal/JiraService.java
+++ src/main/java/org/openengsb/connector/jira/internal/JiraService.java
@@ -389,9 +389,8 @@ public class JiraService extends AbstractOpenEngSBConnectorService implements Is
      */
     private void sendEvent(EDBEventType type, Issue issue) {
         String oid = "jira/" + jiraUser + "/" + issue.getId();
-        String role = "connector";
         try {
-            sendEDBEvent(type, issue, issueEvents, oid, role);
+            sendEDBEvent(type, issue, issueEvents, oid);
         } catch (EDBException e) {
             throw new DomainMethodExecutionException(e);
         }
